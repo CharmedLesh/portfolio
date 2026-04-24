cff7f90a8e3541a5c6bac6410b5351a7d21a4224
CharmedLesh <tur.lesh.5932@gmail.com>
2026-04-24 15:53:13 +0300

    nav styles bugfix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,7 +11,8 @@ export default function Nav() {
 
   useEffect(() => {
     const onScroll = () => setScrolled(window.scrollY > 40);
-    window.addEventListener('scroll', onScroll);
+    onScroll();
+    window.addEventListener('scroll', onScroll, { passive: true });
     return () => window.removeEventListener('scroll', onScroll);
   }, []);
 
